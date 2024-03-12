331441818017dbf9ced2165654782c980ea3b9ee
Julien Levesy <jlevesy@gmail.com>
2024-03-12 15:54:13 +0100

    feat(vehicle): implement delete

--- vehicle/delete.go
+++ vehicle/delete.go
@@ -2,7 +2,9 @@ package vehicle
 
 import (
 	"net/http"
+	"strconv"
 
+	"github.com/AxelDmt/vehicle-server/pkg/httputil"
 	"github.com/AxelDmt/vehicle-server/storage"
 	"go.uber.org/zap"
 )
@@ -20,5 +22,33 @@ func NewDeleteHandler(store storage.Store, logger *zap.Logger) *DeleteHandler {
 }
 
 func (d *DeleteHandler) ServeHTTP(rw http.ResponseWriter, r *http.Request) {
-	http.Error(rw, "Not Implemented", http.StatusInternalServerError)
+	id, err := strconv.ParseInt(r.PathValue("id"), 10, 64)
+	if err != nil {
+		d.logger.Error(
+			"Could not parse vehicle id",
+			zap.Error(err),
+		)
+	}
+
+	ok, err := d.store.Vehicle().Delete(r.Context(), id)
+	if err != nil {
+		d.logger.Error(
+			"Could not delete a vehicle from store",
+			zap.Error(err),
+		)
+
+		httputil.ServeError(rw, http.StatusInternalServerError, err)
+		return
+	}
+
+	if !ok {
+		httputil.ServeJSON(rw, http.StatusNotFound, &httputil.APIError{
+			Code:    httputil.ErrCodeResourceNotFound,
+			Message: "Vehicle not Found",
+		})
+		return
+	}
+
+	rw.WriteHeader(http.StatusNoContent)
+	return
 }
